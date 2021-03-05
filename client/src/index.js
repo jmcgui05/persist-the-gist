9539e60d6bf9f604eae5a8b2205afa7aad8bf300
jmcgui05 <johnmac81@gmail.com>
2021-03-05 16:49:19 -0600

    first clientside commit

---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/api'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+serviceWorker.unregister();
